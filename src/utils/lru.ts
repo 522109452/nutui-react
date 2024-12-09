91e4a784b936df62bfb874e3c48115d57910bdeb
xueyueben <522109452@qq.com>
2024-12-09 14:02:39 +0800

    feat:  增加capacity 参数值校验

--- src/utils/lru.ts
+++ src/utils/lru.ts
@@ -1,14 +1,17 @@
 export default class MiniLru {
-  private cache: Map<string, any>
+  private cache: Map<any, any>
 
   private capacity: number
 
   constructor(capacity: number) {
+    if (capacity <= 0) {
+      throw new Error('Cache capacity must be a positive number')
+    }
     this.cache = new Map()
     this.capacity = capacity
   }
 
-  get(key: string): any | null {
+  get(key: any): any | null {
     if (this.cache.has(key)) {
       const value = this.cache.get(key)
       this.cache.delete(key)
@@ -18,16 +21,16 @@ export default class MiniLru {
     return null
   }
 
-  set(key: string, value: any): void {
+  set(key: any, value: any): void {
     if (this.cache.has(key)) {
       this.cache.delete(key)
     } else if (this.cache.size >= this.capacity) {
-      this.cache.delete(this.cache.keys().next().value as string)
+      this.cache.delete(this.cache.keys().next().value)
     }
     this.cache.set(key, value)
   }
 
-  has(key: string): boolean {
+  has(key: any): boolean {
     return this.cache.has(key)
   }
 }
